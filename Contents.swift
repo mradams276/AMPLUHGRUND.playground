c3de635bec75be5e0ffec3da6bb03406294caaa7
mradams276 <kyle.adams276@gmail.com>
2016-09-15 09:33:13 -0600

    First Swift Project

---
+++ Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "I live in constant fear and misery"
+var welcomeMessage: String
+welcomeMessage = "ALL HAIL BLACK SANTA 🎅🏿"
+
+var integer = 123454321
